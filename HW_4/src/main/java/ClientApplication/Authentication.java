5946ecebf703ba68823b641905930819bb05afaa
Sergei Sokolov <sergsok1984@gmail.com>
2023-11-14 10:46:10 +0300

    add HW_4 to Seminar_4

---
+++ HW_4/src/main/java/ClientApplication/Authentication.java
@@ -0,0 +1,18 @@
+package ClientApplication;
+
+import Core.UserProvider;
+import Models.User;
+
+/**
+ * Класс аутентификации пользователя
+ */
+public class Authentication {
+    public static User authentication(UserProvider userProvider, String login, int passHash) {
+        var client = userProvider.getClientByName(login);
+        if (client.getHashPassword() == passHash) {
+            return client;
+        } else {
+            throw new RuntimeException("Authentication fail");
+        }
+    }
+}
